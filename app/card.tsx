9dce8f87364b84d14420b83cf93c92e3000db772
Daniel <daniel.bailey.dev@gmail.com>
2024-02-21 04:05:52 +1100

    Add project

---
+++ app/card.tsx
@@ -0,0 +1,36 @@
+import Image from "next/image";
+import { useState } from "react";
+
+interface CardProps {
+    imgURL: string;
+    priceNormal: number | null | undefined;
+    priceHolofoil: number | null | undefined;
+    priceReverseHolofoil: number | null | undefined;
+}
+
+export default function Card({ imgURL, priceNormal, priceHolofoil, priceReverseHolofoil }: CardProps) {
+    const [imageError, setImageError] = useState(false);
+
+    const handleImageError = () => {
+        setImageError(true);
+    };
+
+    return (
+        <div className="px-2 py-5">
+            <Image
+                src={imageError ? '/card404.png' : imgURL}
+                alt="Pokemon Image"
+                width={0}
+                height={0}
+                sizes="100vw"
+                priority
+                style={{ width: '300px', height: 'auto' }}
+                onError={handleImageError}
+            />
+        
+            {priceNormal ? <p>Normal: ${priceNormal.toFixed(2)} (USD)</p> : null}
+            {priceHolofoil ? <p>Holofoil: ${priceHolofoil.toFixed(2)} (USD)</p> : null}
+            {priceReverseHolofoil ? <p>Reverse Holofoil: ${priceReverseHolofoil.toFixed(2)} (USD)</p> : null}
+        </div>
+    );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,286 @@
-import Image from "next/image";
+'use client'
+import { PokemonTCG } from 'pokemon-tcg-sdk-typescript';
+import { useEffect, useRef, useState } from 'react';
+
+import Card from './card';
+
+let sortBy = "newest";
+let searchTerm = "";
+let pokemonList: string[] = [];
+const maxCardsPerPage = 20;
+let totalPages = 1;
+let currentPageElement: HTMLSpanElement;
+
+async function getPokemonData() {
+  const apiUrl = "https://pokeapi.co/api/v2/pokemon/?limit=2000";
+
+  try {
+    const response = await fetch(apiUrl);
+
+    if (!response.ok) {
+      throw new Error(`Failed to fetch data from ${apiUrl}`);
+    }
+
+    let results = await response.json();
+    pokemonList = results.results.map((obj: { name: string; }) => obj.name);
+
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
+
+getPokemonData();
 
 export default function Home() {
+  const [searchCards, setSearchCards] = useState<PokemonTCG.Card[]>([]);
+  const [isFocused, setIsFocused] = useState(false);
+  const [noCards, setNoCards] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const searchBar = useRef<HTMLInputElement | null>(null);
+  const searchDropdown = useRef<HTMLUListElement | null>(null);
+  const pagination = useRef<HTMLDivElement | null>(null);
+
+  const handleSearchBarFocus = () => {
+    setIsFocused(true);
+  };
+
+  useEffect(() => {
+    const handleOutSideClick = (event: MouseEvent) => {
+      if (!searchDropdown.current?.contains(event.target as Node) && !searchBar.current?.contains(event.target as Node)) {
+        setIsFocused(false);
+      }
+    };
+
+    window.addEventListener("mousedown", handleOutSideClick);
+
+    return () => {
+      window.removeEventListener("mousedown", handleOutSideClick);
+    };
+  }, [searchDropdown]);
+
+  function createPagination(cardAmount:number) {
+    setCurrentPage(1);
+        totalPages = Math.ceil(cardAmount / maxCardsPerPage);
+
+        if (totalPages !== 1) {
+          for (let i = 1; i <= totalPages; i++) {
+            const paginationText = document.createTextNode(i.toString());
+
+            const paginationItem = document.createElement('span');
+
+
+            if (i === 1) {
+              currentPageElement = paginationItem;
+              currentPageElement.className = 'cursor-pointer px-3 underline text-gray-500';
+            } else {
+              paginationItem.className = 'cursor-pointer px-3 hover:bg-gray-200';
+            }
+
+            paginationItem.onclick = (event) => {
+              setCurrentPage(Number((event.target as HTMLElement).textContent!));
+              currentPageElement.className = 'cursor-pointer px-3 hover:bg-gray-200';
+              currentPageElement = (event.target as HTMLElement);
+              currentPageElement.className = 'cursor-pointer px-3 underline text-gray-500';
+              window.scrollTo({
+                top: 0,
+                behavior: 'smooth'
+              });
+            };
+            paginationItem.appendChild(paginationText);
+
+            pagination.current?.appendChild(paginationItem);
+          }
+        }
+  }
+
+  function getCards() {
+    PokemonTCG.findCardsByQueries({ q: 'name:"' + searchTerm + '"' }).then((cards: PokemonTCG.Card[]) => {
+      if (pagination.current) {
+        pagination.current.innerHTML = '';
+      }
+
+      if (cards.length === 0) {
+        if (searchTerm.includes('-')) {
+          searchTerm = searchTerm.replace('-', '. ');
+          getCards();
+        } else if (searchTerm.includes('. ')) {
+          searchTerm = searchTerm.replace('. ', ' ');
+          getCards();
+        } else if (searchTerm.includes(' ')) {
+          searchTerm = searchTerm.substring(0, searchTerm.indexOf(' '));
+          getCards();
+        }
+        else {
+          setSearchCards([]);
+          setNoCards(true);
+        }
+      } else {
+        setNoCards(false);
+        createPagination(cards.length);
+        setSearchCards(sortCards(cards));
+      }
+    })
+      .catch(error => {
+        console.error(error);
+        throw error;
+      });
+  }
+
+  function sortCards(cards: PokemonTCG.Card[]) {
+    if (sortBy === "oldest" || sortBy === "newest") {
+      cards.sort((a, b) => {
+        const releaseDateA = new Date(a.set.releaseDate).getTime();
+        const releaseDateB = new Date(b.set.releaseDate).getTime();
+        if (sortBy === "oldest") {
+          return releaseDateA - releaseDateB;
+        } else {
+          return releaseDateB - releaseDateA;
+        }
+      });
+      return cards;
+    } else if (sortBy === "highest price" || sortBy === "lowest price") {
+      cards.sort((a, b) => {
+        let highestA;
+        let highestB;
+
+        if (a.tcgplayer?.prices) {
+          const marketA = a.tcgplayer?.prices.normal?.market || 0;
+          const holofoilMarketA = a.tcgplayer?.prices.holofoil?.market || 0;
+          const reverseHolofoilMarketA = a.tcgplayer?.prices.reverseHolofoil?.market || 0;
+
+          highestA = marketA > holofoilMarketA ?
+            (marketA > reverseHolofoilMarketA ? marketA : reverseHolofoilMarketA) :
+            (holofoilMarketA > reverseHolofoilMarketA ? holofoilMarketA : reverseHolofoilMarketA);
+        } else {
+          highestA = 0;
+        }
+
+        if (b.tcgplayer?.prices) {
+          const marketB = b.tcgplayer?.prices.normal?.market || 0;
+          const holofoilMarketB = b.tcgplayer?.prices.holofoil?.market || 0;
+          const reverseHolofoilMarketB = b.tcgplayer?.prices.reverseHolofoil?.market || 0;
+
+          highestB = marketB > holofoilMarketB ?
+            (marketB > reverseHolofoilMarketB ? marketB : reverseHolofoilMarketB) :
+            (holofoilMarketB > reverseHolofoilMarketB ? holofoilMarketB : reverseHolofoilMarketB);
+        } else {
+          highestB = 0;
+        }
+
+        if (sortBy === "highest price") {
+          return highestB - highestA;
+        } else {
+          return highestA - highestB;
+        }
+      });
+      return cards;
+    } else {
+      return cards;
+    }
+
+  }
+
+  function changeSearchTerm(searchString: string) {
+    searchTerm = searchString;
+
+    let suggestions = pokemonList.filter(suggestion =>
+      suggestion.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    if (searchDropdown.current) {
+      searchDropdown.current.innerHTML = '';
+    }
+
+    suggestions.forEach(suggestion => {
+      const text = suggestion;
+      const listItemText = document.createTextNode(text);
+
+      const listItem = document.createElement('li');
+      listItem.className = 'cursor-pointer px-3 hover:bg-gray-100';
+      listItem.onclick = (event) => {
+        if (searchBar.current && event.target) {
+          searchBar.current.value = (event.target as HTMLElement).textContent!;
+          changeSearchTerm(searchBar.current.value);
+          setIsFocused(false);
+          getCards();
+        }
+      };
+      listItem.appendChild(listItemText);
+
+      searchDropdown.current?.appendChild(listItem);
+    });
+  }
+
+  function changeSortBy(sortByString: string) {
+    sortBy = sortByString;
+    setSearchCards(sortCards([...searchCards]));
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <main className="flex min-h-screen flex-col items-center px-3 py-3 sm:px-12 sm:py-12 lg:px-24">
+      <div className="flex flex-col flex-wrap max-w-7xl lg:flex-row lg:justify-center">
+        <div className="flex mb-2 lg:mb-0">
+          <div>
+            <label htmlFor="search" className="sr-only">
+              Search
+            </label>
+            <input
+              className="block rounded-l-lg border border-gray-300 w-52 h-full px-3 py-2 outline-0 focus:border-gray-400 placeholder:text-gray-500 sm:w-80"
+              placeholder="Search Pokemon"
+              onChange={(event) => {
+                changeSearchTerm(event.target.value);
+              }}
+              ref={searchBar}
+              onFocus={handleSearchBarFocus}
             />
+            <ul id="searchDropdown" className={`${!isFocused && 'hidden'} overflow-y-auto w-52 max-h-36 absolute bg-white sm:w-80`}
+              ref={searchDropdown}>
+            </ul>
+          </div>
+          <a
+            className="text-nowrap font-semibold cursor-pointer rounded-r-lg border border-gray-300 px-5 py-2 bg-gray-100 transition-colors hover:border-gray-400 hover:bg-gray-200 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+            rel="noopener noreferrer"
+            onClick={getCards}
+          >
+            Get Cards{" "}
           </a>
+        </div>
+
+        <div className="flex justify-center lg:ml-4">
+          <label className='self-center pr-2' htmlFor="dropdown">Sort By:</label>
+          <select id="dropdown" className='rounded-lg border border-gray-300 py-2 px-3 outline-0 focus:border-gray-400'
+            onChange={(e) => {
+              changeSortBy(e.target.value);
+            }}>
+            <option value="newest">Newest</option>
+            <option value="oldest">Oldest</option>
+            <option value="highest price">Highest Price</option>
+            <option value="lowest price">Lowest Price</option>
+          </select>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="flex justify-center flex-wrap max-w-7xl">
+        {noCards && <h2 className='py-10 text-2xl font-bold'>
+          Card not found
+        </h2>}
+        {searchCards.slice((currentPage - 1) * maxCardsPerPage, (currentPage - 1) * maxCardsPerPage + maxCardsPerPage).map((card) => {
+          if (card.tcgplayer?.prices) {
+            return <Card key={card.id} imgURL={card.images.large}
+              priceNormal={card.tcgplayer?.prices.normal?.market}
+              priceHolofoil={card.tcgplayer?.prices.holofoil?.market}
+              priceReverseHolofoil={card.tcgplayer?.prices.reverseHolofoil?.market} />;
+          } else {
+            return <Card key={card.id} imgURL={card.images.large}
+              priceNormal={null}
+              priceHolofoil={null}
+              priceReverseHolofoil={null} />;
+          }
+        })}
       </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <div className="flex justify-center flex-wrap max-w-7xl" ref={pagination}></div>
     </main>
   );
 }
